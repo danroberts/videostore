89d3e07c25b14b7ccbfe1856aca2027e375325ef
Daniel Roberts <danielrobertsdesign@gmail.com>
2015-03-06 16:29:41 -0500

    Move Method: calculate_price from Rental to Movie

--- lib/movie.rb
+++ lib/movie.rb
@@ -10,4 +10,27 @@ class Movie
     @title = title
     @price_code = price_code
   end
+
+  def price(days_rented)
+    price = 0
+    case @price_code
+      when Movie::REGULAR
+        price += 2
+        if days_rented > 2
+          price += (days_rented - 2) * 1.5
+        end
+      when Movie::NEW_RELEASE
+        price += days_rented * 3
+      when Movie::CHILDRENS
+        price += 1.5
+        if days_rented > 3
+          price += (days_rented - 3) * 1.5
+        end
+      else
+        # do nothing for a while
+    end
+
+    return price
+
+  end
 end
