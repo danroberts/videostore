ab7a1dbdf1523359f6a86f32622f9780d3d637c7
Daniel Roberts <danielrobertsdesign@gmail.com>
2015-03-06 15:47:32 -0500

    Extract method => calculate_price

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,24 +15,7 @@ class Customer
     frequent_renter_points = 0
     result = "Rental record for #{@name}\n"
     for rental in @rentals
-      this_amount = 0
-      case rental.movie.price_code
-        when Movie::REGULAR
-          this_amount += 2
-          if rental.days_rented > 2
-            this_amount += (rental.days_rented - 2) * 1.5
-          end
-        when Movie::NEW_RELEASE
-          this_amount += rental.days_rented * 3
-        when Movie::CHILDRENS
-          this_amount += 1.5
-          if rental.days_rented > 3
-            this_amount += (rental.days_rented - 3) * 1.5
-          end
-        else
-          # do nothing for a while
-      end
-
+      this_amount = calculate_price(rental) 
       frequent_renter_points += 1
       if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
         frequent_renter_points += 1
@@ -45,5 +28,27 @@ class Customer
     result += "Amount owed is $#{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
+  end
+
+  def calculate_price(rental)
+    price = 0
+    case rental.movie.price_code
+      when Movie::REGULAR
+        price += 2
+        if rental.days_rented > 2
+          price += (rental.days_rented - 2) * 1.5
+        end
+      when Movie::NEW_RELEASE
+        price += rental.days_rented * 3
+      when Movie::CHILDRENS
+        price += 1.5
+        if rental.days_rented > 3
+          price += (rental.days_rented - 3) * 1.5
+        end
+      else
+        # do nothing for a while
+    end
+
+    return price
   end
 end
