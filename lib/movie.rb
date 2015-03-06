8292c6f1b0036ece89a5eaa0be823b00b2060bff
Daniel Roberts <danielrobertsdesign@gmail.com>
2015-03-06 16:39:37 -0500

    Replace type code with polymorphism for Movie step 1

--- lib/movie.rb
+++ lib/movie.rb
@@ -20,17 +20,39 @@ class Movie
           price += (days_rented - 2) * 1.5
         end
       when Movie::NEW_RELEASE
-        price += days_rented * 3
+        price += NewReleaseMovie.new(@title, @price_code).price(days_rented)
       when Movie::CHILDRENS
-        price += 1.5
-        if days_rented > 3
-          price += (days_rented - 3) * 1.5
-        end
+        price += ChildrensMovie.new(@title, @price_code).price(days_rented)
       else
         # do nothing for a while
     end
 
     return price
+  end
 
+
+  def frequent_rental_points(days_rented)
+    points = 0
+    if @price_code == Movie::NEW_RELEASE && days_rented > 1
+      return 2
+    else
+      return 1
+    end
+  end
+end
+
+class NewReleaseMovie < Movie
+  def price(days_rented)
+    price = days_rented * 3
+  end
+end
+
+class ChildrensMovie < Movie
+  def price(days_rented)
+    price = 1.5
+    if days_rented > 3
+      price += (days_rented - 3) * 1.5
+    end
+    price
   end
 end
--- lib/rental.rb
+++ lib/rental.rb
@@ -7,33 +7,10 @@ class Rental
   end
 
   def calculate_price
-    price = 0
-    case @movie.price_code
-      when Movie::REGULAR
-        price += 2
-        if @days_rented > 2
-          price += (@days_rented - 2) * 1.5
-        end
-      when Movie::NEW_RELEASE
-        price += @days_rented * 3
-      when Movie::CHILDRENS
-        price += 1.5
-        if @days_rented > 3
-          price += (@days_rented - 3) * 1.5
-        end
-      else
-        # do nothing for a while
-    end
-
-    return price
+    @movie.price(@days_rented)
   end
 
   def calculate_frequent_rental_points
-    points = 0
-    points += 1
-    if @movie.price_code == Movie::NEW_RELEASE && @days_rented > 1
-      points += 1
-    end
-    return points
+    @movie.frequent_rental_points(@days_rented)
   end
 end
